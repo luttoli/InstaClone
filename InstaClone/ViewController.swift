55e55f685c1dace78e36143d21c98200d620874a
luttoli <hhn0212@naver.com>
2024-02-24 14:40:59 +0900

    [FEAT] git 연결

---
+++ InstaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaClone
+//
+//  Created by 김지훈 on 2/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
